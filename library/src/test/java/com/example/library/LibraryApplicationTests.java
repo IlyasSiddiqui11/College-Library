2248b607cece46d7302a6591904822979a9bd313
princ <prince902226062@gmail.com>
2026-06-25 00:00:33 +0530

    fix: add H2 test DB, mock mail sender to fix failing integration test

--- library/src/test/java/com/example/library/LibraryApplicationTests.java
+++ library/src/test/java/com/example/library/LibraryApplicationTests.java
@@ -2,9 +2,14 @@ package com.example.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class LibraryApplicationTests {
+
+	@MockBean
+	JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
