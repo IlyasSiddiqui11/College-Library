4980d158a2c6ca9d2f65b5da1e0a489759d6d7b2
Ilyas Siddiqui <ilyas@example.com>
2026-07-04 20:41:23 +0530

    fix

--- lib_frontend/src/api/client.js
+++ lib_frontend/src/api/client.js
@@ -6,8 +6,7 @@ export const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-  },
-  withCredentials: true,
+  }
 })
 
 apiClient.interceptors.request.use(
