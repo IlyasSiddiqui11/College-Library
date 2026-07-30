0cccbc3c708be0a2e3bebab5f07ab7f4a8382df3
Ilyas Siddiqui <ilyas@example.com>
2026-07-30 21:41:50 +0530

    Frontend Deployment changes

--- lib_frontend/vite.config.js
+++ lib_frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'https://college-library-2sfk.onrender.com',
         changeOrigin: true,
         secure: false,
       }
